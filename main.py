e9b64eb50ab2819acdb95d9704b223d4294def25
crazynayan <nayan@crazyideas.co.in>
2020-07-07 19:01:15 +0530

    Add logging to the update_url cloud function

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from flask_app.file import Image
 
 
 def update_url(_, __):
+    print("Update URL function invoked")
     players = Player.objects.get()
+    print(f"{len(players)} players read")
     url_expiration = dt.datetime.utcnow().replace(tzinfo=pytz.UTC)
     for player in players:
         player.url = Image.url(player.name)
         player.url_expiration = url_expiration
-    Player.save_all(players)
-    print(f"{len(players)} player url updated.")
+    print(f"All URL generated. Now saving all players.")
     print(f"URLs are valid till {url_expiration.isoformat()}")
+    Player.save_all(players)
+    print(f"{len(players)} player url updated. Function complete")
